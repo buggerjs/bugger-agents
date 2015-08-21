ba65635025d6c20031e7c360bffd598323846bc6
Jan Krems <jan.krems@groupon.com>
2015-08-21 09:41:14 -0700

    Add support for live editing

--- example/alive.js
+++ example/alive.js
@@ -1,1 +1,10 @@
 setInterval(function() {}, 1000 * 60 * 60 * 24);
+
+function doStuff() {
+  var x = 20;
+  if (x < process.fauxProperty) {
+    console.log('Weird stuff going on.', __dirname);
+  }
+}
+
+setInterval(doStuff, 2 * 1000);
